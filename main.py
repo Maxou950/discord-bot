2d38dec2ad6ea4e8cff811b6a7c04a6d12341094
Maxou950 <maxencebalduzzi@gmail.com>
2025-07-06 22:09:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -200,6 +200,9 @@ async def cat(ctx):
         description="Un chat si expressif...",
         color=discord.Color.orange()
     )
+    embed.set_image(url="https://media.tenor.com/Bg3ShfbkKJwAAAAC/rigby-cat-rigby.gif")  # ✅ lien direct
+    await ctx.send(embed=embed)
+    )
     embed.set_image(url="https://media1.tenor.com/m/Bg3ShfbkKJwAAAAd/rigby-cat-rigby.gif")
     await ctx.send(embed=embed)
 
